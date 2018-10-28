6270f9201def74e19d017bc198f4ddb974ec7fb6
ArKoshy <arkoshy@gmail.com>
2018-10-28 18:10:42 +0530

    first commit

---
+++ data.py
@@ -0,0 +1,22 @@
+def Faculties():
+    faculties=[
+        {
+        'dept':'CSE',
+         'name': 'Arun',
+         'mob': 8921084158,
+         'email': 'arkoshy@gmail.com'
+        },
+        {
+        'dept':'Civil Engg',
+         'name': 'Ramesh',
+         'mob': 892235262,
+         'email': 'aramesh@gmail.com'
+        },
+        {
+        'dept':'Mechanical Engg',
+         'name': 'Govind',
+         'mob': 892234235,
+         'email': 'govindsh@gmail.com'
+        },
+    ]
+    return faculties
---
+++ pyweb.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+#request is used to read data from HMTL
+from data import Faculties  # to import function
+
+app = Flask(__name__)
+
+getfaculties = Faculties()
+
+## Routing ##
+@app.route('/')  #default route
+def index():
+    return render_template('home.html')   #picks file from template folders
+
+@app.route('/about')
+def about():
+    # return '<h1> About Page </h1>'
+    return render_template('about.html') 
+
+@app.route('/send',methods=['GET','POST'])
+def send():
+    if(request.method =='POST'):                #extract data from form
+        getname=request.form['name']
+        getemail=request.form['email']
+        return render_template('contact.html',name=getname,email=getemail)       
+
+
+@app.route('/faculties')
+def faculties():
+    return render_template('faculties.html',sendDat =getfaculties)
+
+app.run(debug=True)      ## debug=True turns on debug mode
+
